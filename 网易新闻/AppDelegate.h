df0b5e1e800ba630b031600ffee3272daf5b4e2e
pengfeiyiban588 <peng@pengmacbook-pro.local>
2016-06-07 12:38:13 +0800

    Initial Commit

---
+++ 网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by 鹏 on 16/6/7.
+//  Copyright © 2016年 cn.heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易新闻/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  网易新闻
+//
+//  Created by 鹏 on 16/6/7.
+//  Copyright © 2016年 cn.heima. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
